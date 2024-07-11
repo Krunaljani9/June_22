05d581da6525e45797d652e42696feed9ea4b956
Krunaljani9 <krunal.logicraysacademy011223@gmail.com>
2024-07-11 10:17:16 +0530

    First Java Project

---
+++ src/basic/First.java
@@ -0,0 +1,23 @@
+package basic;
+
+/*
+ * if
+ * if else
+ * else if
+ * nested if
+ * if(condition)
+ * */
+public class First {
+	
+	
+	public static void main(String ar[]) {
+	
+		int a=10,b=5,c=7;
+		if(a>b && a>c) {
+	System.out.println("A is max");
+		}
+		
+		
+	}
+
+}
